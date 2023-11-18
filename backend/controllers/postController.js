aa35de7b17595c681a20017fbe51cb796c3ef823
ryangholland <rghcode@gmail.com>
2023-11-18 11:46:12 -0500

    Implement CRUD functionality for postController

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,36 +1,67 @@
 const asyncHandler = require("express-async-handler");
 
+const Post = require("../models/postModel");
+
 // @desc    Get posts
 // @route   GET /api/posts
 // @access  ?
 const getPosts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "GET posts" });
+  const posts = await Post.find();
+
+  res.status(200).json(posts);
 });
 
 // @desc    Create posts
 // @route   POST /api/posts
 // @access  ?
 const createPost = asyncHandler(async (req, res) => {
+  if (!req.body.title) {
+    res.status(400);
+    throw new Error("Please add a title.");
+  }
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field.");
   }
 
-  res.status(200).json({ message: "CREATE post" });
+  const post = await Post.create({
+    title: req.body.title,
+    text: req.body.text,
+  });
+
+  res.status(200).json(post);
 });
 
 // @desc    Update post
 // @route   PUT /api/posts/:id
 // @access  ?
 const updatePost = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `UPDATE post ${req.params.id}` });
+  const post = await Post.findById(req.params.id);
+
+  if (!post) {
+    res.status(400);
+    throw new Error("Post not found.");
+  }
+
+  const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedPost);
 });
 
 // @desc    Delete post
 // @route   DELETE /api/posts/:id
 // @access  ?
 const deletePost = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `DELETE post ${req.params.id}` });
+  const post = await Post.findById(req.params.id);
+
+  if (!post) {
+    res.status(400);
+    throw new Error("Post not found.");
+  }
+
+  await post.deleteOne();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
---
+++ backend/models/postModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please add a title"],
+    },
+    text: {
+      type: String,
+      required: [true, "Please add text"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Post", postSchema);
