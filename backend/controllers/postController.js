b4283c1a6bf5673f09b5d339f6cc7c1a842d75b5
ryangholland <rghcode@gmail.com>
2023-11-18 09:55:54 -0500

    Create postController and move placeholder functions from postRoutes

---
+++ backend/controllers/postController.js
@@ -0,0 +1,34 @@
+// @desc    Get posts
+// @route   GET /api/posts
+// @access  ?
+const getPosts = (req, res) => {
+  res.status(200).json({ message: "GET posts" });
+};
+
+// @desc    Create posts
+// @route   POST /api/posts
+// @access  ?
+const createPost = (req, res) => {
+  res.status(200).json({ message: "CREATE post" });
+};
+
+// @desc    Update post
+// @route   PUT /api/posts/:id
+// @access  ?
+const updatePost = (req, res) => {
+  res.status(200).json({ message: `UPDATE post ${req.params.id}` });
+};
+
+// @desc    Delete post
+// @route   DELETE /api/posts/:id
+// @access  ?
+const deletePost = (req, res) => {
+  res.status(200).json({ message: `DELETE post ${req.params.id}` });
+};
+
+module.exports = {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+};
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,20 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} = require("../controllers/postController");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "GET posts" });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "CREATE post" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `UPDATE post ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `DELETE post ${req.params.id}` });
-});
+router.route("/").get(getPosts).post(createPost);
+router.route("/:id").put(updatePost).delete(deletePost);
 
 module.exports = router;
