6e6ed946b464549b967d23f3ee35d1c3b6b4c537
ryangholland <rghcode@gmail.com>
2023-11-18 10:30:40 -0500

    Connect to mongoDB

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get posts
 // @route   GET /api/posts
 // @access  ?
-const getPosts = (req, res) => {
+const getPosts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "GET posts" });
-};
+});
 
 // @desc    Create posts
 // @route   POST /api/posts
 // @access  ?
-const createPost = (req, res) => {
+const createPost = asyncHandler(async (req, res) => {
   if (!req.body.text) {
-    res.status(400)
-    throw new Error('Please add a text field.')
+    res.status(400);
+    throw new Error("Please add a text field.");
   }
 
   res.status(200).json({ message: "CREATE post" });
-};
+});
 
 // @desc    Update post
 // @route   PUT /api/posts/:id
 // @access  ?
-const updatePost = (req, res) => {
+const updatePost = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `UPDATE post ${req.params.id}` });
-};
+});
 
 // @desc    Delete post
 // @route   DELETE /api/posts/:id
 // @access  ?
-const deletePost = (req, res) => {
+const deletePost = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `DELETE post ${req.params.id}` });
-};
+});
 
 module.exports = {
   getPosts,
