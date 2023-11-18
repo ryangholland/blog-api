d01c59724fabc137144f32cc90fffbc4e0ed1c3d
ryangholland <rghcode@gmail.com>
2023-11-18 10:13:00 -0500

    Create custom errorHandler

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -9,6 +9,11 @@ const getPosts = (req, res) => {
 // @route   POST /api/posts
 // @access  ?
 const createPost = (req, res) => {
+  if (!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field.')
+  }
+
   res.status(200).json({ message: "CREATE post" });
 };
 
