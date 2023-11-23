87487d97c5cbeaf278f12e7a0bf85b332fb6a5dd
ryangholland <rghcode@gmail.com>
2023-11-23 09:42:53 -0500

    Fix network error when submitting registration form

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,13 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
